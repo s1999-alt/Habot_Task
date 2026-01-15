e970711912d2e40f8b9dee65534b713e517b48a8
Siyad Savad <siyadsavad313@gmail.com>
2026-01-16 01:43:22 +0530

    .env file not reading problem solved with load_dotenv installation and configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- backend/employees/tests.py
+++ backend/employees/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import Employee
 
-# Create your tests here.
+
+class EmployeeTests(APITestCase):
+
+  def test_create_employee(self):
+    data = {
+      "name": "adam",
+      "email": "adam@example.com"
+    }
+    response = self.client.post('api/employees/', data)
+    self.assertEqual(response.status_code, 201)
+  
+  def test_duplicate_email(self):
+    Employee.objects.create(name="A", email="a@test.com")
+    response = self.client.post('/api/employees/', {
+      "name": "B",
+      "email": "a@test.com"
+    })
+    self.assertEqual(response.status_code, 400)
+
+
+
+
+
+
+
+
