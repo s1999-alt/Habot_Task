fc986c5f4ebedc3cce9f638b7e70872e58e521bc
Siyad Savad <siyadsavad313@gmail.com>
2026-01-15 23:52:11 +0530

    created url in accounts url + employee model created + employee serializer created

--- backend/employees/models.py
+++ backend/employees/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField(unique=True)
+  department = models.CharField(max_length=50, blank=True)
+  role = models.CharField(max_length=50, blank=True)
+  date_joined = models.DateField(auto_now_add=True)
+
+  def __str__(self):
+      return self.name
+  
+
