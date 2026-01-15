ab17cc9c4ffacc5d93e57bcc2540c0320774de64
Siyad Savad <siyadsavad313@gmail.com>
2026-01-16 01:09:45 +0530

    url created for employees

--- backend/employees/urls.py
+++ backend/employees/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import EmployeeListCreateAPIView, EmployeeDetailAPIView
 
 urlpatterns = [
-    
+  path('employees/', EmployeeListCreateAPIView.as_view(), name='employees-list-create'),
+  path('employees/<int:id>', EmployeeDetailAPIView.as_view(), name='employees-detail'),
 ]
