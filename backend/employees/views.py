5b4188698227311927f4c63df21ef6428322cd27
Siyad Savad <siyadsavad313@gmail.com>
2026-01-16 00:34:02 +0530

    employee listing and creating view created

--- backend/employees/views.py
+++ backend/employees/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.paginator import Paginator
 
-# Create your views here.
+from .models import Employee
+from .serializers import EmployeeSerializer
+
+
+
+class EmployeeListCreateAPIView(APIView):
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get(self, request):
+    employees = Employee.objects.all()
+
+    department = request.GET.get('department')
+    role = request.GET.get('role')
+
+    if department:
+      employees = employees.filter(department=department)
+    if role:
+      employees = employees.filter(role=role)
+
+    paginator = Paginator(employees, 10)
+    page = request.GET.get('page', 1)
+    employees = paginator.get_page(page)
+
+    serializer = EmployeeSerializer(employees, many=True)
+    return Response(serializer.data)
+  
+  def post(self, request):
+    serializer = EmployeeSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+
+
+
+
